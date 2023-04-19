97d9c7a64f66b5020a98d273cbb58b194dce3588
Ramon <ramon.r.santana@gmail.com>
2023-04-19 10:25:22 +0200

    feat: Add inputText that I will later use in the rendering for validations

---
+++ src/Components/InputText/InputText.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './InputText.css'
+
+export const InputText = ({
+    className,
+    type,
+    name,
+    placeholder,
+    required,
+    changeFunction,
+    blurFunction,
+}) => {
+    return (
+        <>
+        <Form.Control
+            className={className}
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            required={required}
+            onChange={(e) => changeFunction(e)}
+            onBlur={(e) => blurFunction(e)}
+        />
+        </>
+    );
+};

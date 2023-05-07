c46381c586ec8450d78e872052e51e4e685169df
Ramon <ramon.r.santana@gmail.com>
2023-05-07 11:44:27 +0200

    chore: Change vite config

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import store from "./app/store";
+import store from "./app/store.js";
 
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from 'redux-persist';
